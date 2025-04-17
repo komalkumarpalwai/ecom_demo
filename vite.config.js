b8a6420ee7580a25f790abf5445b14b75872a13a
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2025-04-17 22:47:32 +0530

    first commit final

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Allow external network access
-    port: 3000, // You can change this to any port you prefer
+    port: 3000, // Change this to your preferred port if needed
+    allowedHosts: [
+      'ecom-demo-0c3u.onrender.com', // Add this host to the allowed list
+    ],
   },
 })
